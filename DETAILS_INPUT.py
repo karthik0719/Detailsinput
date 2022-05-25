39007c5e658e91aebf77750595cbc90c36aeb319
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-25 16:25:33 +0530

    Add files via upload

---
+++ DETAILS_INPUT.py
@@ -0,0 +1,19 @@
+"""
+Name: Karthik
+Date: 19th May 2022
+Day: Thursday
+Time: 7:00-8:00PM
+File Description:  Details_input
+"""
+Name=input("Enter your name ")
+empid=int(input('Enter your EMPID '))
+Salary=float(input("Enter your Salary "))
+
+# print('My name is'+" "+Name+" "+"and my empid is"+" "+str(empid))
+# print("My salary for the month is"+" "+str(Salary)+"$")
+
+# print('My name is {} '"and my empid is {} ".format(Name,empid))
+# print("My salary for the month is {} ".format(Salary))
+
+# print('My name is {1} '"and my empid is {0} ".format(empid,Name))
+# print("My salary for the month is {0}cls ".format(Salary))
